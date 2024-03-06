23b6dcd5a1a1bea961bcaac626dd4b37b84eb0cf
Anna Kioko <annakioko00@gmail.com>
2024-03-06 07:53:22 +0300

    completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,18 @@
+CREATE TABLE bears (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT,
+age INTEGER,
+sex TEXT,
+color TEXT,
+temperament TEXT,
+alive BOOLEAN
+);
+
+--INSERT INTO bears (name, sex) VALUES("Mr.Chocolate", "M" );
+--INSERT INTO bears (name, sex) VALUES('Rowdy', 'M');
+--INSERT INTO bears (name, sex) VALUES('Tabitha', 'F');
+--INSERT INTO bears (name, sex) VALUES('Sergeant Brown', 'M');
+--INSERT INTO bears (name, sex) VALUES('Melissa', 'F');
+--INSERT INTO bears (name, sex) VALUES('Grinch', 'M');
+--INSERT INTO bears (name, sex) VALUES('Wendy', 'F');
+--INSERT INTO bears (name, sex) VALUES('NULL','F');
